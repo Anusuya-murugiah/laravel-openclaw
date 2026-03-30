3d44903afc32605cece95b0bfa327404dd0723d5
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2026-03-30 18:10:58 +0530

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -1,12 +1,1 @@
-<?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-use App\Http\Controllers\UserController;
-
-Route::post('/user/create', [UserController::class, 'createUser']); 
-Route::get('/users', [UserController::class, 'getUsers']);
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
