f6425ca85e7babc16d30d2839a7a0c2c8d6a67ed
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2026-03-30 18:03:49 +0530

    Update Controller.php

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,32 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class UserController extends Controller
 {
-    //
+    public function createUser(Request $request)
+    {
+        $data = $request->all();
+
+        $password = $data['password'];
+
+        $query = "INSERT INTO users (name, email, password) VALUES ('"
+            . $data['name'] . "', '"
+            . $data['email'] . "', '"
+            . $password . "')";
+
+        DB::insert($query);
+
+        return response()->json([
+            "message" => "User created",
+            "password" => $password
+        ]);
+    }
+
+    public function getUsers()
+    {
+        return DB::select("SELECT * FROM users");
+    }
 }
